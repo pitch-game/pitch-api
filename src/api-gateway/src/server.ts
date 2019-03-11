9b9b7f34359a67bd7976ffacff67145330da4a43
Jacob <jcbcn@users.noreply.github.com>
2019-03-11 12:44:32 +0000

    Initial commit for express-gateway

--- src/api-gateway/src/server.ts
+++
@@ -1,9 +0,0 @@
-import express = require('express');
-
-const app = express();
-
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('Hello World');
-});
-
-app.listen(3000);
