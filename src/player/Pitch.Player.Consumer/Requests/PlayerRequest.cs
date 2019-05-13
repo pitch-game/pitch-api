f2d1f285a56881648edc74452f5b7dab96706172
Jacob <jcbcn@users.noreply.github.com>
2019-05-13 22:45:52 +0100

    Initial commit for EasyNetQ in player service

---
+++ src/player/Pitch.Player.Consumer/Requests/PlayerRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Pitch.Player.Consumer.Messages
+{
+    public class PlayerRequest
+    {
+        public PlayerRequest((int? lower, int? upper) ratingRange, string position = null)
+        {
+            RatingRange = ratingRange;
+            Position = position;
+        }
+
+        //E.g. 50 - 99
+        public (int? lower, int? upper) RatingRange { get; private set; }
+        public string Position { get; private set; }
+    }
+}
