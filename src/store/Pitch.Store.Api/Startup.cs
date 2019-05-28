ea3134c8f708d3b43707c1d7f033859f8c0ff978
Jacob <jcbcn@users.noreply.github.com>
2019-05-28 23:13:50 +0100

    Should be HTTPS

--- src/store/Pitch.Store.Api/Startup.cs
+++ src/store/Pitch.Store.Api/Startup.cs
@@ -45,7 +45,7 @@ namespace Pitch.Store.Api
 
             }).AddJwtBearer(options =>
             {
-                options.Authority = "http://localhost.pitch-game.io/identity";
+                options.Authority = "https://localhost.pitch-game.io/identity";
                 //options.MetadataAddress = "https://localhost.pitch-game.io/identity/.well-known/openid-configuration";
                 options.Audience = "cbf24cc4a1bb79e441a5b5937be6dd84";
                 options.RequireHttpsMetadata = false;
