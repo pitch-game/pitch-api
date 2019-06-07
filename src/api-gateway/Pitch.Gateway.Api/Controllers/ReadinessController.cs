020357b0d9da6bd0b8f11d801d512decf91bf596
Jacob <jcbcn@users.noreply.github.com>
2019-06-07 21:49:58 +0100

    Readiness controller

---
+++ src/api-gateway/Pitch.Gateway.Api/Controllers/ReadinessController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pitch.Gateway.Api.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class ReadinessController : ControllerBase
+    {
+        // GET /
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
