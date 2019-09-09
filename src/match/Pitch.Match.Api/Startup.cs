c937eef2db7178f3cc24f46c140cd729de04e4fe
Jacob <jcbcn@users.noreply.github.com>
2019-09-09 16:16:11 +0100

    Update Startup.cs

--- src/match/Pitch.Match.Api/Startup.cs
+++ src/match/Pitch.Match.Api/Startup.cs
@@ -63,8 +63,8 @@ namespace Pitch.Match.Api
 
             services.AddHealthChecks()
                 .AddCheck("self", () => HealthCheckResult.Healthy())
-                .AddRabbitMQ(Configuration.GetConnectionString("RabbitMQHealthCheck"), name: "rabbitmq-check", tags: new string[] { "rabbitmq" });
-                //.AddMongoDb(Configuration.GetConnectionString("MongoDb"), name: "mongodb-check", tags: new string[] { "mongodb" });
+                .AddRabbitMQ(Configuration.GetConnectionString("RabbitMQHealthCheck"), name: "rabbitmq-check", tags: new string[] { "rabbitmq" })
+                .AddMongoDb(Configuration.GetConnectionString("MongoDb"), name: "mongodb-check", tags: new string[] { "mongodb" });
             //.AddSignalRHub("/hubs/matchmaking", name: "signalr-check", tags: new string[] { "signalr" });
 
             services.AddSingleton<IMongoClient>(s =>
