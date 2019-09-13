d3c5a915559f5e199d251c0c02e8da87153661c9
Jacob <jcbcn@users.noreply.github.com>
2019-09-13 17:30:35 +0100

    Unit tests for repo and dbcontext

--- src/match/Pitch.Match.Api.Tests/MongoDbDataContextTests.cs
+++ src/match/Pitch.Match.Api.Tests/MongoDbDataContextTests.cs
@@ -17,7 +17,7 @@ namespace Pitch.Match.Api.Tests
         }
 
         [Fact]
-        public async Task CreateAsync_TestEntity_MongoInsertOneASyncIsCalled()
+        public async Task CreateAsync_TestEntity_CalledOnceMongoInsertOneAsync()
         {
             var testId = Guid.NewGuid();
             var testEntity = new TestEntity()
@@ -40,6 +40,32 @@ namespace Pitch.Match.Api.Tests
 
             //Assert
             mongoCollectionMock.Verify(m => m.InsertOneAsync(testEntity, null, It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task UpdateAsync_TestEntity_CalledOnceMongoReplaceOneAsync()
+        {
+            var testId = Guid.NewGuid();
+            var testEntity = new TestEntity()
+            {
+                Id = testId
+            };
+
+            var mongoCollectionMock = new Mock<IMongoCollection<TestEntity>>();
+            mongoCollectionMock.Setup(x => x.InsertOneAsync(It.IsAny<TestEntity>(), null, It.IsAny<CancellationToken>()));
+
+            var mongoDatabaseMock = new Mock<IMongoDatabase>();
+            mongoDatabaseMock.Setup(x => x.GetCollection<TestEntity>(It.IsAny<string>(), null)).Returns(mongoCollectionMock.Object);
+
+            var mongoClientMock = new Mock<IMongoClient>();
+            mongoClientMock.Setup(x => x.GetDatabase(It.IsAny<string>(), null)).Returns(mongoDatabaseMock.Object);
+
+            // Act
+            var mongoDbContext = new MongoDbDataContext<TestEntity>(mongoClientMock.Object);
+            await mongoDbContext.UpdateAsync(testEntity);
+
+            //Assert
+            mongoCollectionMock.Verify(m => m.ReplaceOneAsync(It.IsAny<FilterDefinition<TestEntity>>(), testEntity, null, It.IsAny<CancellationToken>()), Times.Once);
         }
     }
 }
--- src/match/Pitch.Match.Api.Tests/Repositories/MatchRepositoryTests.cs
+++ src/match/Pitch.Match.Api.Tests/Repositories/MatchRepositoryTests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Moq;
+using Pitch.Match.Api.ApplicationCore.Models;
 using Pitch.Match.Api.Infrastructure.Repositories;
 using Pitch.Match.Api.Infrastructure.Repositories.Contexts;
 using Xunit;
@@ -10,26 +13,188 @@ namespace Pitch.Match.Api.Tests.Repositories
 {
     public class MatchRepositoryTests
     {
+        public class InMemoryDataContext<T> : IDataContext<T> where T : IEntity
+        {
+            public InMemoryDataContext(List<T> inMemoryList = null)
+            {
+                InMemoryList = inMemoryList ?? new List<T>();
+            }
+
+            private IList<T> InMemoryList { get; set; }
+
+            public Task CreateAsync(T item)
+            {
+                InMemoryList.Add(item);
+                return Task.CompletedTask;
+            }
+
+            public Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> query)
+            {
+                return Task.FromResult(InMemoryList.FirstOrDefault(query.Compile()));
+            }
+
+            public Task<IEnumerable<T>> ToListAsync(Expression<Func<T, bool>> query)
+            {
+                return Task.FromResult(InMemoryList.Where(query.Compile()));
+            }
+
+            public Task UpdateAsync(T item)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
         [Fact]
         public async Task GetAsync_MatchIdGuidPassed_ReturnsMatchWithId()
         {
             // Arrange
             var matchId = Guid.NewGuid();
-
             var stubMatch = new ApplicationCore.Models.Match
             {
                 Id = matchId
             };
+            var stubMatchList = new List<ApplicationCore.Models.Match>() {stubMatch};
 
-            var mockDataContext = new Mock<IDataContext<ApplicationCore.Models.Match>>();
-            mockDataContext.Setup(x => x.FirstOrDefaultAsync(It.IsAny<Expression<Func<ApplicationCore.Models.Match, bool>>>())).Returns(Task.FromResult(stubMatch));
-            
+            var dbContextStub = new InMemoryDataContext<ApplicationCore.Models.Match>(stubMatchList);
+
             // Act
-            var repository = new MatchRepository(mockDataContext.Object);
+            var repository = new MatchRepository(dbContextStub);
             var result = await repository.GetAsync(matchId);
 
             //Assert
             Assert.Equal(stubMatch, result);
+        }
+
+        [Fact]
+        public async Task GetInProgressAsync_WithMatchInProgressAsHomeTeam_ReturnsMatchId()
+        {
+            // Arrange
+            var matchId = Guid.NewGuid();
+            var userId = Guid.NewGuid();
+            var stubMatch = new ApplicationCore.Models.Match
+            {
+                Id = matchId,
+                HomeTeam = new TeamDetails()
+                {
+                    UserId = userId
+                },
+                AwayTeam =  new TeamDetails()
+                {
+                    UserId = Guid.NewGuid()
+                },
+                KickOff = DateTime.Now.AddMinutes(-10)
+            };
+            var stubMatchList = new List<ApplicationCore.Models.Match>() { stubMatch };
+
+            var dbContextStub = new InMemoryDataContext<ApplicationCore.Models.Match>(stubMatchList);
+
+            // Act
+            var repository = new MatchRepository(dbContextStub);
+            var result = await repository.GetInProgressAsync(userId);
+
+            //Assert
+            Assert.Equal(stubMatch.Id, result);
+        }
+
+        [Fact]
+        public async Task GetInProgressAsync_WithMatchInProgressAsAwayTeam_ReturnsMatchId()
+        {
+            // Arrange
+            var matchId = Guid.NewGuid();
+            var userId = Guid.NewGuid();
+            var stubMatch = new ApplicationCore.Models.Match
+            {
+                Id = matchId,
+                HomeTeam = new TeamDetails()
+                {
+                    UserId = Guid.NewGuid()
+                },
+                AwayTeam = new TeamDetails()
+                {
+                    UserId = userId
+                },
+                KickOff = DateTime.Now.AddMinutes(-10)
+            };
+            var stubMatchList = new List<ApplicationCore.Models.Match>() { stubMatch };
+
+            var dbContextStub = new InMemoryDataContext<ApplicationCore.Models.Match>(stubMatchList);
+
+            // Act
+            var repository = new MatchRepository(dbContextStub);
+            var result = await repository.GetInProgressAsync(userId);
+
+            //Assert
+            Assert.Equal(stubMatch.Id, result);
+        }
+
+        [Fact]
+        public async Task GetInProgressAsync_WithCompletedMatch_ReturnsNull()
+        {
+            // Arrange
+            var matchId = Guid.NewGuid();
+            var userId = Guid.NewGuid();
+            var stubMatch = new ApplicationCore.Models.Match
+            {
+                Id = matchId,
+                HomeTeam = new TeamDetails()
+                {
+                    UserId = Guid.NewGuid()
+                },
+                AwayTeam = new TeamDetails()
+                {
+                    UserId = userId
+                },
+                KickOff = DateTime.Now.AddMinutes(-100)
+            };
+            var stubMatchList = new List<ApplicationCore.Models.Match>() { stubMatch };
+
+            var dbContextStub = new InMemoryDataContext<ApplicationCore.Models.Match>(stubMatchList);
+
+            // Act
+            var repository = new MatchRepository(dbContextStub);
+            var result = await repository.GetInProgressAsync(userId);
+
+            //Assert
+            Assert.Equal((Guid?)null, result);
+        }
+
+        [Fact]
+        public async Task GetInProgressAsync_WithNoMatchInProgress_ReturnsNull()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var stubMatchList = new List<ApplicationCore.Models.Match>();
+
+            var dbContextStub = new InMemoryDataContext<ApplicationCore.Models.Match>(stubMatchList);
+
+            // Act
+            var repository = new MatchRepository(dbContextStub);
+            var result = await repository.GetInProgressAsync(userId);
+
+            //Assert
+            Assert.Equal((Guid?)null, result);
+        }
+
+
+        [Fact]
+        public async Task CreateAsync_CallsCreateASyncOnDbContextOnce()
+        {
+            // Arrange
+            var matchId = Guid.NewGuid();
+            var stubMatch = new ApplicationCore.Models.Match()
+            {
+                Id = matchId
+            };
+
+            var dbContextMock = new Mock<IDataContext<ApplicationCore.Models.Match>>();
+            dbContextMock.Setup(x => x.CreateAsync(stubMatch));
+
+            // Act
+            var repository = new MatchRepository(dbContextMock.Object);
+            var result = await repository.CreateAsync(stubMatch);
+
+            //Assert
+            dbContextMock.Verify(x => x.CreateAsync(stubMatch), Times.Once);
         }
     }
 }
