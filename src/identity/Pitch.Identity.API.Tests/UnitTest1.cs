42f9384d563179a312de69f11609281dad9c9826
Jacob <jcbcn@users.noreply.github.com>
2019-09-14 16:26:08 +0100

    Remove docker-compose from identity and setup unit tests and draft

---
+++ src/identity/Pitch.Identity.API.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Pitch.Identity.API.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
