d2c8fabfd04daf7064d278a3fcbf7afde6a63e9d
Jacob <jcbcn@users.noreply.github.com>
2019-09-15 14:06:40 +0100

    Unit tests for squad

---
+++ src/squad/Pitch.Squad.API.Tests/SimpleTypeNameSerializerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Pitch.Squad.API.Supporting;
+using Xunit;
+
+namespace Pitch.Squad.API.Tests
+{
+    public class SimpleTypeNameSerializerTest
+    {
+        public class TestObject {}
+
+        [Fact]
+        public void DeSerialize_ReturnsCorrectType()
+        {
+            //Arrange
+            var types = new Type[] { typeof(TestObject) };
+
+            //Act
+            var serializer = new SimpleTypeNameSerializer(types);
+
+            //Assert
+            Assert.Equal(typeof(TestObject), serializer.DeSerialize("TestObject"));
+        }
+
+        [Fact]
+        public void Serialize_ReturnsCorrectString()
+        {
+            //Arrange
+            var types = new Type[] { typeof(TestObject) };
+
+            //Act
+            var serializer = new SimpleTypeNameSerializer(types);
+
+            //Assert
+            Assert.Equal("TestObject", serializer.Serialize(typeof(TestObject)));
+        }
+    }
+}
