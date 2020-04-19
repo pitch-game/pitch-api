b87aec2b5cb298edc46afcbf0e83a0c0dd319afd
jcbcn <jcbcn@users.noreply.github.com>
2020-04-19 14:43:50 +0100

    Upgrade Store API to dotnet core 3.1

--- src/store/Pitch.Store.API/Startup.cs
+++ src/store/Pitch.Store.API/Startup.cs
@@ -1,4 +1,8 @@
-﻿using EasyNetQ;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using EasyNetQ;
 using EasyNetQ.AutoSubscribe;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -6,113 +10,99 @@ using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.IdentityModel.Logging;
+using Microsoft.OpenApi.Models;
+using MongoDB.Driver;
 using Pitch.Store.API.Application.Subscribers;
 using Pitch.Store.API.Infrastructure;
 using Pitch.Store.API.Infrastructure.Repositories;
 using Pitch.Store.API.Infrastructure.Services;
 using Pitch.Store.API.Supporting;
-using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Reflection;
-using MongoDB.Driver;
 using Swashbuckle.AspNetCore.Swagger;
 
-namespace Pitch.Store.API
-{
+namespace Pitch.Store.API {
     [ExcludeFromCodeCoverage]
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices (IServiceCollection services) {
             IdentityModelEventSource.ShowPII = true;
 
-            services.AddHealthChecks()
-                .AddCheck("self", () => HealthCheckResult.Healthy())
-                .AddUrlGroup(new Uri(Configuration["CardHealthCheckUrl"]), name: "cardapi-check", tags: new string[] { "cardapi" })
-                .AddRabbitMQ(Configuration.GetConnectionString("RabbitMQHealthCheck"), name: "rabbitmq-check", tags: new string[] { "rabbitmq" });
+            services.AddHealthChecks ()
+                .AddCheck ("self", () => HealthCheckResult.Healthy ())
+                .AddUrlGroup (new Uri (Configuration["CardHealthCheckUrl"]), name: "cardapi-check", tags : new string[] { "cardapi" })
+                .AddRabbitMQ (Configuration.GetConnectionString ("RabbitMQHealthCheck"), name: "rabbitmq-check", tags : new string[] { "rabbitmq" });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_3_0);
 
-            services.AddAuthentication(options =>
-            {
+            services.AddAuthentication (options => {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 
-            }).AddJwtBearer(options =>
-            {
-                options.Authority = Configuration.GetValue<string>("IdentityUrl");
+            }).AddJwtBearer (options => {
+                options.Authority = Configuration.GetValue<string> ("IdentityUrl");
                 options.Audience = "cbf24cc4a1bb79e441a5b5937be6dd84";
                 options.RequireHttpsMetadata = false;
             });
 
-            services.AddScoped<IPackService, PackService>();
-            services.AddScoped<IPackRepository, PackRepository>();
-            services.AddScoped(typeof(IDataContext<>), typeof(MongoDbDataContext<>));
+            services.AddScoped<IPackService, PackService> ();
+            services.AddScoped<IPackRepository, PackRepository> ();
+            services.AddScoped (typeof (IDataContext<>), typeof (MongoDbDataContext<>));
 
-            services.AddScoped<IUserCreatedEventSubscriber, UserCreatedEventSubscriber>();
-            services.AddScoped<ISubscriber, UserCreatedEventSubscriber>();
-            services.AddScoped<IMatchCompletedEventSubscriber, MatchCompletedEventSubscriber>();
-            services.AddScoped<ISubscriber, MatchCompletedEventSubscriber>();
+            services.AddScoped<IUserCreatedEventSubscriber, UserCreatedEventSubscriber> ();
+            services.AddScoped<ISubscriber, UserCreatedEventSubscriber> ();
+            services.AddScoped<IMatchCompletedEventSubscriber, MatchCompletedEventSubscriber> ();
+            services.AddScoped<ISubscriber, MatchCompletedEventSubscriber> ();
 
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor> ();
 
-            services.AddSingleton(s =>
-            {
-                var typesInAssembly = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes()).ToArray();
-                return RabbitHutch.CreateBus(Configuration.GetConnectionString("ServiceBus"), serviceRegister =>
-                    serviceRegister.Register<ITypeNameSerializer>(serviceProvider => new SimpleTypeNameSerializer(typesInAssembly)));
+            services.AddSingleton (s => {
+                var typesInAssembly = AppDomain.CurrentDomain.GetAssemblies ().SelectMany (x => x.GetTypes ()).ToArray ();
+                return RabbitHutch.CreateBus (Configuration.GetConnectionString ("ServiceBus"), serviceRegister =>
+                    serviceRegister.Register<ITypeNameSerializer> (serviceProvider => new SimpleTypeNameSerializer (typesInAssembly)));
             });
 
-            services.AddSingleton<IMongoClient>(s =>
-            {
-                return new MongoClient(Configuration.GetConnectionString("MongoDb"));
+            services.AddSingleton<IMongoClient> (s => {
+                return new MongoClient (Configuration.GetConnectionString ("MongoDb"));
             });
 
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new Info { Title = "Store API", Version = "v1" });
+            services.AddSwaggerGen (c => {
+                c.SwaggerDoc ("v1", new OpenApiInfo { Title = "Store API", Version = "v1" });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseHsts ();
             }
 
-            app.UseSwagger();
+            app.UseSwagger ();
 
-            app.UseHealthChecks("/health");
-            app.UseHealthChecks("/liveness", new HealthCheckOptions
-            {
-                Predicate = r => r.Name.Contains("self")
+            app.UseHealthChecks ("/health");
+            app.UseHealthChecks ("/liveness", new HealthCheckOptions {
+                Predicate = r => r.Name.Contains ("self")
             });
 
-            app.UseEasyNetQ();
-            app.UseAuthentication();
-            app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseEasyNetQ ();
+            app.UseAuthentication ();
+            app.UseHttpsRedirection ();
+            app.UseRouting ();
+
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
+            });
         }
     }
 }
