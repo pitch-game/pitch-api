0f09ccc41122360ba2ad68d7bcf2f19ab684b644
jcbcn <jcbcn@users.noreply.github.com>
2020-04-25 12:57:12 +0100

    Upgrade test project packages

--- src/user/Pitch.User.API/Startup.cs
+++ src/user/Pitch.User.API/Startup.cs
@@ -35,7 +35,7 @@ namespace Pitch.User.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped(typeof(IDataContext<>), typeof(MongoDbDataContext<>));
