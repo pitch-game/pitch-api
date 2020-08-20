ae8759b2bcbbecf99baf0b2bdab0c42e39ede0ef
Jacob <jcbcn@users.noreply.github.com>
2020-08-20 20:04:17 +0100

    Install nginx-ingress via bitnami helm chart

--- deploy/k8s/terraform/modules/ingress/main.tf
+++ deploy/k8s/terraform/modules/ingress/main.tf
@@ -1,6 +1,18 @@
 provider "kubernetes" {
 }
 
+resource "helm_release" "nginx-ingress" {
+  name       = "nginx-ingress"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+  namespace  = "pitch-api"
+
+  set {
+    name  = "rbac.create"
+    value = true
+  }
+}
+
 resource "kubernetes_ingress" "pitch-gateway-ingress" {
   metadata {
     name      = "pitch-gateway-ingress"
