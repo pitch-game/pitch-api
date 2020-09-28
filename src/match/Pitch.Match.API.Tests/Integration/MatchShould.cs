43b40599e7ac3e85b256f0927a12eb7c5d0356cc
Jacob <jcbcn@users.noreply.github.com>
2020-09-28 21:21:06 +0100

    Initial commit

---
+++ src/match/Pitch.Match.API.Tests/Integration/MatchShould.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Pitch.Match.API.Tests.Integration
+{
+    public class MatchShould : IClassFixture<IntegrationTestsFixture>
+    {
+        private readonly IntegrationTestsFixture _fixture;
+
+        public MatchShould(IntegrationTestsFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task Return_BadRequest_When_Id_Isnt_Guid()
+        {
+            var client = _fixture.CreateClient();
+
+            var result = await client.GetAsync("/1");
+            result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task Integration_test_uses_decorator()
+        {
+            var client = _fixture.CreateClient();
+
+            var result = await client.GetAsync("/db694e59-ae5c-4e32-989c-e17b0ead81a2");
+            var response = await result.Content.ReadAsStringAsync();
+            result.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ src/match/Pitch.Match.API.Tests/IntegrationTestsFixture.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Pitch.Match.API.ApplicationCore.Models;
+using Pitch.Match.API.ApplicationCore.Models.Match;
+using Pitch.Match.API.Infrastructure.Repositories.Contexts;
+using Pitch.Match.API.Tests.Integration;
+
+namespace Pitch.Match.API.Tests
+{
+    public class IntegrationTestsFixture : WebApplicationFactory<Startup>
+    {
+        private readonly Mock<IDataContext<ApplicationCore.Models.Match.Match>> _mockDataContext;
+
+        public IntegrationTestsFixture()
+        {
+            _mockDataContext = new Mock<IDataContext<ApplicationCore.Models.Match.Match>>();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+
+            //TODO default match builder
+            var matchId = new Guid("db694e59-ae5c-4e32-989c-e17b0ead81a2");
+            var match = new ApplicationCore.Models.Match.Match()
+            {
+                Id = matchId,
+                KickOff = DateTime.Now.AddMinutes(-5),
+                HomeTeam = new TeamDetails()
+                {
+                    Squad = new Squad()
+                },
+                AwayTeam = new TeamDetails()
+                {
+                    Squad = new Squad()
+                }
+            };
+
+            _mockDataContext.Setup(x => x.FindOneAsync(x => x.Id == matchId))
+                .ReturnsAsync(match);
+
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddAuthentication(options =>
+                    { 
+                        options.DefaultAuthenticateScheme = "Test"; 
+                        options.DefaultChallengeScheme = "Test";
+                    })
+                    .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(
+                        "Test", options => { });
+
+                services.AddScoped((sp) => _mockDataContext.Object);
+            });
+        }
+    }
+}
